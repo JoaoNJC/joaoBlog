2b4510f23bbebde910a0f299a773cb8297ed970e
João Camba <joaonoeljose@gmail.com>
2023-07-16 17:14:04 +0100

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: "class",
   theme: {
   },
-  plugins: [require("tailwindcss/typography")],
+  plugins: [],
 }
